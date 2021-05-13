88ab4be28c0c6089ec144e6f1e0635ff09b4be22
mjyurich <matthewyurich@gmail.com>
2021-05-13 13:56:08 -0400

    PythonChallenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,14 @@ import csv
 #Create path for resources get data
 csvpath = os.path.join(".", "resources", "budget_data.csv")
 
-#Variables needed 
+#Variables/Lists parameters needed
 months = []
+revenue_list = []
 total_months = 0
 net_total = 0
-total_change = 0
+revenue_last = 0
+
+
 
 #Open and read the data
 with open(csvpath, "r") as csvfile:
@@ -24,7 +27,13 @@ with open(csvpath, "r") as csvfile:
         total_months = total_months + 1
         net_total = net_total + int(row[1])
 
-        
+       #Determine the changes in Profits/Losses
+        revenue_change = int(row[1]) - revenue_last
+        revenue_last = int(row[1])
+        revenue_list = revenue_list + [revenue_change]
+
+        #Calculate the Average revenue change
+        revenue_average = sum(revenue_list) / len(revenue_list)
 
         
 
